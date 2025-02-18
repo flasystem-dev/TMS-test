0531f87945234110224c76141a948be58c5e597a
root <root@ip-172-31-59-75.ap-northeast-2.compute.internal>
2025-02-18 10:53:14 +0900

    250218 큐 테스트 추가

--- app/Console/Commands/QueueWorkCustom.php
+++ app/Console/Commands/QueueWorkCustom.php
@@ -3,10 +3,13 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Queue\Console\WorkCommand;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
-class QueueWorkCustom extends WorkCommand
+
+
+class QueueWorkCustom extends Command
 {
     /**
      * The name and signature of the console command.
@@ -27,19 +30,37 @@ class QueueWorkCustom extends WorkCommand
      */
     public function handle()
     {
+        // 제외할 큐 가져오기
         $exceptQueue = $this->option('except');
-        $queueList = explode(',', $this->option('queue'));
 
+        // 현재 DB에 존재하는 큐 가져오기
+        $queueList = DB::table('jobs')
+            ->select('queue')
+            ->distinct()
+            ->pluck('queue')
+            ->toArray();
+
+        // 제외할 큐가 있다면 필터링
         if ($exceptQueue) {
             $queueList = array_filter($queueList, function ($queue) use ($exceptQueue) {
                 return $queue !== $exceptQueue;
             });
         }
 
-        $this->input->setOption('queue', implode(',', $queueList));
+        // 실행할 큐 리스트 만들기
+        $queueNames = implode(',', $queueList);
 
-        Log::info("Queue Worker started with queues: " . implode(',', $queueList));
+        if (empty($queueNames)) {
+            Log::info("No queues available to process.");
+            return;
+        }
 
-        parent::handle();
+        Log::info("Starting queue worker with queues: " . $queueNames);
+
+        // Laravel의 기본 queue:work 명령어 실행
+        Artisan::call('queue:work', [
+            '--queue' => $queueNames,
+            '--tries' => 3,
+        ]);
     }
 }
